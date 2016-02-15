cfdabe3910d63466094176cdcdbae51c4278ced4
= <=>
2016-02-15 16:06:12 +0100

    more models

--- app/models/Address.scala
+++ app/models/Address.scala
@@ -1,32 +1,29 @@
 package models
 
-import anorm._
-import play.api.libs.ws._
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
-import scala.concurrent.Future
-import org.bitcoinj.params.MainNetParams
-import org.bitcoinj.core.AddressFormatException
+import play.api.libs.ws._
 
-case class Address(address:String,balance:Long) extends Model 
-case class AddressesInfo(count: Int, balance: Long) extends Info
-case class RichestInfo(inAddresses: Long, inWallets: Long, total: Long, count: Int)
+case class Address(address:String,balance:Long) 
+case class AddressesSummary(count: Int, balance: Long)
 
 object Address  
 {
   implicit val addressReads = Json.reads[Address]
 
-  def getRichList(blockHeight: Int, table: String): Future[List[Address]] = {
-    val from = if (table == "richest_addresses") "addresses" else "wallets"
-    WS.url("http://bitcoinprivacy.net:8080/richlist/"+from+"/1/1000").get().map {response => (response.json).as[List[Address]]}
-  }
+  def getRichList(blockHeight: Int, table: String) = 
+    getFromApi("richlist", table, "1", "1000").map(_.json.as[List[Address]])
 
-  def getAddressesPage(hex:String, height: Int) = Future{Pagination(100,100)}
+  def getWallet(hex:String, height: Int, from: Int, to: Int) =
+    getFromApi("wallet", hex, from.toString, to.toString) map {response => (response.json).as[List[Address]]}
 
-  def getAddressesInfo(hex: String, height: Int) = Future{AddressesInfo(1,19)}
+}
 
-  def getRichestInfo(height: Int) = Future{RichestInfo(1,2,3,4)}
+object AddressesSummary{
 
-  def getAddresses(hex:String, height: Int,page: Int) =WS.url("http://bitcoinprivacy.net:8080/wallet/"+hex+"/"+pageSize*(page-1)+ "/"+pageSize*page).get().map {response => (response.json).as[List[Address]]}
-  
+  implicit val adsumReads = Json.reads[AddressesSummary]
+
+  def get(hex: String, height: Int) = getFromApi("wallet", hex, "summary") map (_.json.as[AddressesSummary])
+
+  def getRichest(height: Int, table: String) = getFromApi("richlist",table,"summary") map (_.json.as[AddressesSummary])
+
 }
--- app/models/UTXO.scala
+++ app/models/UTXO.scala
@@ -1,19 +1,14 @@
 package models
 
-import anorm._
-import play.api.libs.ws._
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
-import scala.concurrent.Future
-import org.bitcoinj.params.MainNetParams
-import org.bitcoinj.core.AddressFormatException
+import play.api.libs.ws._
 
 
 case class UTXO(tx: String, value:Long, address: String)
 case class UTXOsSummary(count: Int, sum: Long)
 
-object UTXO
-{
+object UTXO {
+
   implicit val utxosReads = Json.reads[UTXO]
 
   def getFromAddress(address: String, from: Int, to: Int) =
@@ -21,19 +16,17 @@ object UTXO
 
   def getFromTx(tx: String, from: Int, to: Int) =
     getFromApi("tx_utxos", tx, from.toString, to.toString).map(_.json.as[List[UTXO]])
+
 }
 
-object UTXOsSummary
-  {
+object UTXOsSummary {
 
   implicit val utxossummary = Json.reads[UTXOsSummary]
 
-    def getFromAddress(address: String) =
-      getFromApi("utxos", address, "summary")
+  def getFromAddress(address: String) =
+    getFromApi("utxos", address, "summary")
 
-    def getFromTx(tx: String) =
-      getFromApi("tx_utxos", tx, "summary")
-  }
+  def getFromTx(tx: String) =
+    getFromApi("tx_utxos", tx, "summary")
 
-
-
+}
