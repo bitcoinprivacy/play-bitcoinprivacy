d9ea030e6503472455cbb516adb5cd91a8fd09cf
= <=>
2016-02-15 15:05:21 +0100

    UTXO added

---
+++ app/models/UTXO.scala
@@ -0,0 +1,39 @@
+package models
+
+import anorm._
+import play.api.libs.ws._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import scala.concurrent.Future
+import org.bitcoinj.params.MainNetParams
+import org.bitcoinj.core.AddressFormatException
+
+
+case class UTXO(tx: String, value:Long, address: String)
+case class UTXOsSummary(count: Int, sum: Long)
+
+object UTXO
+{
+  implicit val utxosReads = Json.reads[UTXO]
+
+  def getFromAddress(address: String, from: Int, to: Int) =
+    getFromApi("utxos", address, from.toString, to.toString).map(_.json.as[List[UTXO]])
+
+  def getFromTx(tx: String, from: Int, to: Int) =
+    getFromApi("tx_utxos", tx, from.toString, to.toString).map(_.json.as[List[UTXO]])
+}
+
+object UTXOsSummary
+  {
+
+  implicit val utxossummary = Json.reads[UTXOsSummary]
+
+    def getFromAddress(address: String) =
+      getFromApi("utxos", address, "summary")
+
+    def getFromTx(tx: String) =
+      getFromApi("tx_utxos", tx, "summary")
+  }
+
+
+
